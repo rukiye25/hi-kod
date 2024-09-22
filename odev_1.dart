4f46ee1c2d957a3cb7cc02c6c37af441f7ab91db
Rukiye Naz Yazgan <77140579+rukiye25@users.noreply.github.com>
2024-09-22 11:50:42 +0300

    Add files via upload

---
+++ odev_1.dart
@@ -0,0 +1,33 @@
+void main() {
+  int deger_1 = 10;
+  String deger_2 = "deger 2";
+  double deger_3 = 2.5;
+  bool deger_4 = true;
+  String chardeger = "A";
+  print(deger_1);
+  print(deger_2);
+  print(deger_3);
+  print(deger_4);
+  print(chardeger);
+
+  //camelcase
+  int sayiDegeri = 10;
+  print(sayiDegeri);
+  //snake_case
+  String deger_string = "deger 2";
+  print(deger_string);
+  //PascalCase
+  double DegerDouble = 2.5;
+  print(DegerDouble);
+
+  //isim
+  String isim = "rukiye";
+  //yas
+  int yas = 19;
+  //resit mi
+  bool resit = true;
+
+  print(isim);
+  print(yas);
+  print(resit);
+}
